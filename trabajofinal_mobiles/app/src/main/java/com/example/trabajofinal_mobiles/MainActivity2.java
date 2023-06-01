353fc1e963b90e3426200acc2ee2767598ff6815
AndrexMed <giovanniandrex@gmail.com>
2023-05-31 20:38:55 -0500

    Templates trabajoFinal_mobiles1

--- trabajofinal_mobiles/app/src/main/java/com/example/trabajofinal_mobiles/MainActivity2.java
+++ trabajofinal_mobiles/app/src/main/java/com/example/trabajofinal_mobiles/MainActivity2.java
@@ -2,7 +2,9 @@ package com.example.trabajofinal_mobiles;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity2 extends AppCompatActivity {
 
@@ -11,4 +13,16 @@ public class MainActivity2 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
     }
+
+    public void cartagena(View view){
+        Intent intent = new Intent(this, MainActivity3.class);
+        startActivity(intent);
+    }
+
+    public void servicios(View view){
+        Intent intent = new Intent(this, MainActivity4.class);
+        startActivity(intent);
+    }
+
+
 }
