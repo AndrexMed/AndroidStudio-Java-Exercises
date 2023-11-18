562678f9955cdd0b6a2b9962dfc06d9151070154
lDanteZ <129919990+lDanteZ@users.noreply.github.com>
2023-11-18 16:23:42 -0500

    Front

---
+++ appRentCarsFBFS/app/src/main/java/com/example/apprentcarsfbfs/Return_car.java
@@ -0,0 +1,14 @@
+package com.example.apprentcarsfbfs;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Return_car extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_return_car);
+    }
+}
