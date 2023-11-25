d4ae9365bca8edb9376fa9d119cf4fced03879d6
lDanteZ <129919990+lDanteZ@users.noreply.github.com>
2023-11-25 15:34:06 -0500

    Fin

--- appRentCarsFBFS/app/src/main/java/com/example/apprentcarsfbfs/CarRegister.java
+++ appRentCarsFBFS/app/src/main/java/com/example/apprentcarsfbfs/CarRegister.java
@@ -16,6 +16,7 @@ import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.HashMap;
@@ -29,6 +30,8 @@ public class CarRegister extends AppCompatActivity {
     Button btCreate, btRead, btUpdate, btDelete;
 
     FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+    String idAutomatic;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,7 +55,134 @@ public class CarRegister extends AppCompatActivity {
             }
         });
 
+        btRead.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String plateNumber = etPlateNumber.getText().toString();
+                if (!plateNumber.isEmpty()){
+                    searchCar(plateNumber);
+                } else{
+                    Toast.makeText(CarRegister.this, "All fields are required", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
 
+        btUpdate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+              updateCar();
+            }
+        });
+
+        btDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String plateNumberToDelete = etPlateNumber.getText().toString();
+                
+                if(!plateNumberToDelete.isEmpty()){
+                    deleteUserInFS(plateNumberToDelete);
+                } else {
+                    Toast.makeText(CarRegister.this, "PlateNumber is required", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    private void deleteUserInFS(String plateNumberToDelete) {
+        db.collection("Cars")
+                .whereEqualTo("PlateNumber", plateNumberToDelete)
+                .get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            if (!task.getResult().isEmpty()) {
+                                for (QueryDocumentSnapshot document : task.getResult()) {
+                                    String idAutomatic = document.getId();
+                                    db.collection("Cars").document(idAutomatic).delete().addOnSuccessListener(new OnSuccessListener<Void>() {
+                                        @Override
+                                        public void onSuccess(Void unused) {
+                                            Toast.makeText(CarRegister.this, "Car deleted!", Toast.LENGTH_SHORT).show();
+                                        }
+                                    });
+                                }
+                            } else {
+                                Toast.makeText(CarRegister.this, "Car not found to eliminated", Toast.LENGTH_SHORT).show();
+                            }
+                        } else {
+                            Toast.makeText(CarRegister.this, "Internal error", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+    private void updateCar() {
+
+        String plateNumberToUpdate = etPlateNumber.getText().toString();
+        String brandToUpdate = etBrand.getText().toString();
+        String dailyValueToUpdate = etValue.getText().toString();
+        boolean stateToUpdate = btnState.isChecked();
+
+        if(!plateNumberToUpdate.isEmpty()){
+            updateCarInFS(plateNumberToUpdate, brandToUpdate, dailyValueToUpdate, stateToUpdate);
+        } else {
+            Toast.makeText(CarRegister.this, "All fields are required", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void updateCarInFS(String plateNumberToUpdate, String brandToUpdate, String dailyValueToUpdate, boolean stateToUpdate) {
+        db.collection("Cars")
+                .whereEqualTo("PlateNumber", plateNumberToUpdate)
+                .get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            if (!task.getResult().isEmpty()) {
+                                for (QueryDocumentSnapshot document : task.getResult()) {
+                                    String idAutomatic = document.getId();
+                                    Map<String, Object> dataUser = new HashMap<>();
+                                    dataUser.put("PlateNumber", plateNumberToUpdate);
+                                    dataUser.put("Brand", brandToUpdate);
+                                    dataUser.put("State", stateToUpdate);
+                                    dataUser.put("DailyValue", dailyValueToUpdate);
+
+                                    db.collection("Cars").document(idAutomatic).set(dataUser).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                        @Override
+                                        public void onComplete(@NonNull Task<Void> task) {
+                                            Toast.makeText(CarRegister.this, "Car Updated", Toast.LENGTH_SHORT).show();
+                                        }
+                                    });
+                                }
+                            } else {
+                                Toast.makeText(CarRegister.this, "Car not found to updated", Toast.LENGTH_SHORT).show();
+                            }
+                        } else {
+                            Toast.makeText(CarRegister.this, "Internal Error", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+    private void searchCar(String plateNumber) {
+        db.collection("Cars").whereEqualTo("PlateNumber", plateNumber).get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if (task.isSuccessful()) {
+                    if (!task.getResult().isEmpty()) {
+                        for (QueryDocumentSnapshot document : task.getResult()) {
+                            idAutomatic = document.getId();
+                            etBrand.setText(document.getString("Brand"));
+                            etValue.setText(document.getString("DailyValue"));
+                            btnState.setChecked(document.getBoolean("State"));
+                        }
+                    } else {
+                        Toast.makeText(CarRegister.this, "Car not found", Toast.LENGTH_SHORT).show();
+                    }
+
+                } else {
+                    Toast.makeText(CarRegister.this, "Internal Error", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 
     private void registerCar() {
@@ -99,5 +229,9 @@ public class CarRegister extends AppCompatActivity {
                 //}
             }
         });
+    }
+
+    private void clearData(){
+
     }
 }
